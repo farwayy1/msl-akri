57e2a0aeaada8f520c1da8efcf30473de25bff03
farwayy1 <fikarhazmi0102@gmail.com>
2026-07-10 00:25:54 +0700

    Implement countdown timer to target date
    Added a countdown timer to display days, hours, minutes, and seconds until the target date.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const targetDate = new Date("2026-08-05T07:00:00").getTime();
 
+setInterval(function(){
+
+const now = new Date().getTime();
+
+const distance = targetDate-now;
+
+document.getElementById("days").innerHTML=Math.floor(distance/(1000*60*60*24));
+
+document.getElementById("hours").innerHTML=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+
+document.getElementById("minutes").innerHTML=Math.floor((distance%(1000*60*60))/(1000*60));
+
+document.getElementById("seconds").innerHTML=Math.floor((distance%(1000*60))/1000);
+
+},1000);
